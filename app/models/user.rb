c2f829a3a0df35a78c278feef3156a9f836c4420
Heidi Boulter <boulter.heidi@gmail.com>
2019-12-03 14:57:13 -0600

    theme

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_many :donations
     has_many :children, through: :donations
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :username, length: { minimum: 7 }
+    validates :password, presence: true
+    validates :password, length: { in: 6..20 }
 end
