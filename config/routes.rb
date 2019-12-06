292eba81d24b9fef05e47e7b0953f117efb30348
github username <github email address>
2019-12-06 10:38:25 -0600

    navbar link

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :children
   resources :users
 
+  get     "/sign_out" => "clearance/sessions#destroy", as: "logout"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
